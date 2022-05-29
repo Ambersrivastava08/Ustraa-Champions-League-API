dd2d1e3befd15ffd421b5f1cef877ad6dd4b009c
Ambersrivastava08 <49266154+Ambersrivastava08@users.noreply.github.com>
2022-05-29 14:28:05 +0530

    model logic and admin showcase

--- app/migrations/0001_initial.py
+++ app/migrations/0001_initial.py
@@ -10,7 +10,7 @@ class Migration(migrations.Migration):
     dependencies = [
     ]
 
-    operations = [
+    operations = [ 
         migrations.CreateModel(
             name='Team',
             fields=[
